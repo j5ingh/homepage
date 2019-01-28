b066a8c290031f169724ff457b915caba2e28f4d
Jagmit Singh <jagmit.singh@gmx.de>
2019-01-28 22:49:19 +0100

    preparation for letter display

--- sketch.js
+++ sketch.js
@@ -1,5 +1,14 @@
+let prisma
+
+function preload() {
+  prisma = loadFont('assets/Prisma.ttf');
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight)
+
+  textFont(prisma)
+  textAlign(CENTER, CENTER)
 }
 
 function draw() {
@@ -7,14 +16,24 @@ function draw() {
   for(i = 0; i < 500; i++){
     drawStar()
   }
+
+  drawLetter()
+
   noLoop()
 }
 
-function drawStar(value){
+function drawStar() {
   noStroke()
   fill(random(50,150))
   h = random(0, width)
   w = random(0, height)
   s = random(1, 5)
   ellipse(h, w, s, s)
+}
+
+function drawLetter() {
+  textSize(350)
+  fill(256)
+  text('L', width/2, height/2)
+
 }
