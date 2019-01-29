3b0150ae4f02c1019ecfe60319bd6715a568cbd5
Jagmit Singh <jagmit.singh@gmx.de>
2019-01-29 02:35:49 +0100

    additions

--- ascii.py
+++ ascii.py
@@ -1,16 +1,20 @@
 import random
 import re
+import os
 
 def randAscii():
     num = random.randrange(33, 127)
     ascii = str(chr(num))
+    print("generated new ascii {}".format(ascii))
     return ascii
 
 def updateJS(ascii):
-    filename = "sketch.js"
-    file = open(filename, "r")
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    filename = "/sketch.js"
+    file = open(dir_path + filename, "r")
     lines = file.readlines()
     file.close()
+    print("read js")
 
     p = re.compile('.*let letter = "(.)".*')
 
@@ -18,9 +22,11 @@ def updateJS(ascii):
     for line in lines:
         m = p.match(line)
         if m:
-            line = f'let letter = "{ascii}"\n'
+            line = 'let letter = "{}"\n'.format(ascii)
+            print("updated asii from {} to {}. line is now:\n{}".format(m.group(1), ascii, line))
         file.write(line)
     file.close();
+    print("wrote js")
 
 if __name__ == '__main__':
     updateJS(randAscii())
--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 crontab -l > asciijob
 #echo new cron into cron file
 #echo "0 0 1 * * python3 ascii.py" >> asciijob
-echo "* * * * * python3 ascii.py" >> asciijob
+echo "* * * * * python3 $PWD/$(dirname $0)/ascii.py" >> asciijob
 #install new cron file
 crontab asciijob
 rm asciijob
