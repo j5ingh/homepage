c961a16944ddc9af62ceecb6b138ebb4eaa9829e
Jagmit Singh <jagmit.singh@gmx.de>
2019-01-30 01:25:32 +0100

    run cronjob only once in a month

--- install.sh
+++ install.sh
@@ -2,8 +2,7 @@
 #write out current crontab
 crontab -l > asciijob
 #echo new cron into cron file
-#echo "0 0 1 * * python3 ascii.py" >> asciijob
-echo "* * * * * python3 $PWD/$(dirname $0)/ascii.py" >> asciijob
+echo "0 0 1 * * python3 $PWD/$(dirname $0)/ascii.py" >> asciijob
 #install new cron file
 crontab asciijob
 rm asciijob
